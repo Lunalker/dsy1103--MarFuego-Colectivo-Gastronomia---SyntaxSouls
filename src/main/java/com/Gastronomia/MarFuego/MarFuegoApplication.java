d3f32509b459aed06363ca11d1552c79017bc617
Maty-5 <matiasjeldres2005@gmail.com>
2026-04-21 09:48:56 -0400

    prueba lol

---
+++ src/main/java/com/Gastronomia/MarFuego/MarFuegoApplication.java
@@ -0,0 +1,13 @@
+package com.Gastronomia.MarFuego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarFuegoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarFuegoApplication.class, args);
+	}
+
+}
