e2f9dc1b4e93947097c1964f540e841ea5c10430
Lunalker <deinerquero1@gmail.com>
2026-05-15 17:26:55 -0400

    Corecciones

--- ms-caja/src/test/java/com/marfuego/ms_caja/MsCajaApplicationTests.java
+++ ms-caja/src/test/java/com/marfuego/ms_caja/MsCajaApplicationTests.java
@@ -1,18 +1,10 @@
-<<<<<<<< HEAD:ms-caja/src/test/java/com/marfuego/ms_caja/MsCajaApplicationTests.java
 package com.marfuego.ms_caja;
-========
-package com.Gastronomia.MarFuego;
->>>>>>>> master:ms-menu/src/test/java/com/Gastronomia/MarFuego/MarFuegoApplicationTests.java
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-<<<<<<<< HEAD:ms-caja/src/test/java/com/marfuego/ms_caja/MsCajaApplicationTests.java
 class MsCajaApplicationTests {
-========
-class MarFuegoApplicationTests {
->>>>>>>> master:ms-menu/src/test/java/com/Gastronomia/MarFuego/MarFuegoApplicationTests.java
 
 	@Test
 	void contextLoads() {
--- ms-inventario/src/main/java/com/marfuego/ms_inventario/controller/ControllerInventario.java
+++ ms-inventario/src/main/java/com/marfuego/ms_inventario/controller/ControllerInventario.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/inventario")
+@RequestMapping("/api/v2/inventario")
 public class ControllerInventario {
     @Autowired
     private ServiceInventario service;
